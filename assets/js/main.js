ed08aa1f9cb03044f06554478e93e90caa304981
Tomy Vo <127685331+tomyvo@users.noreply.github.com>
2025-12-26 03:03:16 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -220,3 +220,23 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+
+//======================================== DARK MODE TOGGLE ========================================
+
+const toggle = document.getElementById("themeToggle");
+const body = document.body;
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-mode");
+    toggle.textContent = "☀️";
+  }
+
+toggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    const isDark = body.classList.contains("dark-mode");
+    toggle.textContent = isDark ? "☀️" : "🌙";
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+});
